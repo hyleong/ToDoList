47246f76f93ab00c52622472fda641de4d6da34f
Vincent Leung <i@hyleong.com>
2015-06-17 00:15:13 +0800

    Add New Items

--- ToDoList/ToDoListTableTableViewController.m
+++ ToDoList/ToDoListTableTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "ToDoListTableTableViewController.h"
 #import "ToDoItem.h"
+#import "AddToDoItemViewController.h"
 
 @interface ToDoListTableTableViewController ()
 
@@ -31,7 +32,12 @@
 }
 
 - (IBAction)unwindToList:(UIStoryboardSegue *)segue{
-
+    AddToDoItemViewController *source = [segue sourceViewController];
+    ToDoItem *item = source.toDoItem;
+    if (item != nil) {
+        [self.toDoItems addObject:item];
+        [self.tableView reloadData];
+    }
 }
 
 - (void)viewDidLoad {
